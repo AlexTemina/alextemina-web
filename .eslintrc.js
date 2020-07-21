524676ca2414f6f64dfbf9819c98e4a64f5dc537
alextemina-sopra <alejandro.ruiz2@soprasteria.com>
2020-07-21 10:14:25 +0200

    fix(): lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    'plugin:prettier/recommended',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', '@vue/airbnb'],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -18,7 +13,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
     indent: ['error', 2],
-    '@typescript-eslint/explicit-function-return-type': ['error'],
     'max-len': ['error', { code: 100 }],
     'arrow-body-style': 'off',
     'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
@@ -28,6 +22,31 @@ module.exports = {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
+      },
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+      env: { browser: true, es6: true, node: true },
+      extends: [
+        'plugin:vue/essential',
+        'plugin:prettier/recommended',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+      ],
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+      rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'import/prefer-default-export': 'off',
+        'import/extensions': 'off',
+        indent: ['error', 2],
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        'max-len': ['error', { code: 100 }],
+        'arrow-body-style': 'off',
+        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
--- tests/unit/views/CurriculumVitae/CvModule.spec.ts
+++ tests/unit/views/CurriculumVitae/CvModule.spec.ts
@@ -1,6 +1,6 @@
-import CvModule from '@/views/CurriculumVitae/CvModule.vue';
+import InfoModule from '@/views/CurriculumVitae/components/InfoModule.vue';
 import { render } from '@testing-library/vue';
 
 test('increments value on click', async () => {
-  render(CvModule);
+  render(InfoModule);
 });
